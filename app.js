624421d307150a5c0315ea21159f080bf6e08035
Lytween <victoire.lytween@gmail.com>
2023-10-12 16:57:51 +0200

    fini

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Toggle 
 let btn = document.getElementById("btn_toggle");
 let btnIcon = document.getElementById("btn_icon");
 let reponse = document.getElementById('reponse');
@@ -13,3 +14,8 @@ const toggleResponse = () => {
     
 };
 btn.addEventListener("click", toggleResponse);
+
+//Carrousel
+
+// J'ai pas eu le temps de le faire, j'ai pas reussi à le faire en Bootstrap et je voulais le
+// faire en Javascript. J'ai perdu trop de temps avec Bootstrap donc il est en display none :( 
